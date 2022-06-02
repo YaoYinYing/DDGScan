21fb377fbc0d93333c87c90f57dbf6b6ea0f3d9c
yaoyy.hi@gmail.com <33014714+YaoYinYing@users.noreply.github.com>
2022-06-02 10:05:58 +0800

    fix rosetta mpi, local only; fix rosetta db

--- utils/grape_phaseI.py
+++ utils/grape_phaseI.py
@@ -472,7 +472,7 @@ def main1(args):
             exe_dict["cartddg"] = cartesian_ddg_exe
             break
     relax_exe = os.popen("which relax.mpi.linuxgccrelease").read().replace("\n", "")
-    rosettadb = os.popen("echo $ROSETTADB").read().replace("\n", "")
+    rosettadb = os.popen("echo $ROSETTA3_DB").read().replace("\n", "")
     if not rosettadb:
         rosettadb = "/".join(relax_exe.split("/")[:-4]) + "/database/"
     exe_dict["relax"] = relax_exe
--- utils/rosetta.py
+++ utils/rosetta.py
@@ -58,7 +58,7 @@ class Rosetta:
         relax_threads = min([int(self.threads), int(self.relax_num)])
         relax_cmd = "".join(
             [
-                "mpirun --allow-run-as-root -n "
+                "mpirun --use-hwthread-cpus -np "
                 + str(relax_threads)
                 + " relax.mpi.linuxgccrelease -s "
                 + self.pdbname
